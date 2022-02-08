f7b8d5c5245ee4197db67b21ee0122c5efd39de9
Trever Schirmer <trever@umbreller.de>
2022-02-08 10:32:04 +0100

    :art: Remove unused code in main.tf

--- function/handler.js
+++ function/handler.js
@@ -25,14 +25,16 @@ const fusionSetup = {
 }
 
 exports.handler = async function (event) {
+    // This root handler might be invoked sync or async - we don't really care. Maybe the response will fall into the void.
     console.log('Event: ', event)
+    console.log('Env: ', process.env)
 
-    // let stepName = event.resource.substring(1)
-    // let input = event.body
+    // const [baseUrl, basePath] = getUrlsFromEnv()
 
-    // await invokeLocal(event, stepName, input)
+    // see ExampleLog.md for possible invocation types
+    // let [stepName, input] = getStepNameAndInputFromEvent(event)
 
-    // console.log("Function Setup:", fusionSetup)
+    // let result = await invokeLocal(event, stepName, input)
 
     return {
         statusCode: 200,
@@ -40,7 +42,8 @@ exports.handler = async function (event) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            originalEvent: event,
+            inputEvent: event,
+            result: "result",
             environ: process.env,
         }),
     }
@@ -60,30 +63,31 @@ function getHandler(resource) {
  * @param {*} step  The next step to call - will be appended to base url
  * @param {*} data  The data to call the next function with
  */
-async function invokeRemote(event, step, data, sync=false) {
+async function invokeRemote(step, data, sync=false) {
     return new Promise((resolve, reject) => {
 
-        let invocationType = sync ? "RequestResponse" : "Event"
+        let invocationType = sync ? "SYNC-" : ""
 
         const options = {
-            host: event.headers.Host,
-            path: `/${event.requestContext.stage}/${step}`,
+            host: baseUrl,
+            // onlyStage/SYNC-A to call A sync
+            // onlyStage/A to call A async
+            path: `/${basePath}/${invocationType}${step}`,
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                'X-Amz-Invocation-Type': invocationType,
+                'Content-Type': 'application/json'
             }
         }
         console.log("Sending Request:", options)
         console.time('reqstart')
         let req = https.request(options, (res) => {
             console.log("Request Success", res)
-            data = ''
+            resultData = ''
             res.on('data', (d) => {
-                data += d
+                resultData += d
             })
             res.on('end', () => {
-                resolve(JSON.parse(data))
+                resolve(JSON.parse(resultData))
             })
         })
         req.on('error', (e) => {
@@ -114,12 +118,33 @@ async function invokeLocal(event, stepName, input) {
 
         if (nextStep.appended) {
             console.log("This step should happen here! Calling it...")
-            await invokeLocal(event, nextStepName, result)
+            return invokeLocal(event, nextStepName, result)
         } else {
-            console.log("This step should happen remotely! Firing off a Request.")
-            let remoteResult = await invokeRemote(event, nextStepName, result)
-            console.log("Was the request sent?")
+            console.log(`This step should happen remotely! Firing off a Request. NextStep=${nextStep}`)
+            let remoteResult = await invokeRemote(event, nextStepName, result, nextStep.sync)
+            console.log("Answer from the next Invocation: (HTTP 202 => async)")
             console.log(remoteResult)
+            return remoteResult
         }
     }
+}
+
+function getStepNameAndInputFromEvent(event) {
+    // set from terraform
+    let name = process.env["function_to_handle"]
+
+    // If the event has the property requestContext we assume its a (sync) HTTP request
+    if (event.requestContext) {
+        return [name, event.body]
+    } else {
+        return [name, event]
+    }
+}
+
+function getUrlsFromEnv() {
+    // set from terraform
+    let env = process.env["stage_url"]
+    let lastSlash = env.lastIndexOf("/")
+    // everything before the last slash , everything after the last slash
+    return [env.substring(0, lastSlash), env.substring(lastSlash + 1)]
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -104,78 +104,6 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-// API Gateway to call Lambda
-
-# resource "aws_apigatewayv2_api" "lambda" {
-#   name          = "serverless_lambda_gw"
-#   protocol_type = "HTTP"
-# }
-
-# resource "aws_apigatewayv2_stage" "lambda" {
-#   api_id = aws_apigatewayv2_api.lambda.id
-
-#   name        = "serverless_lambda_stage"
-#   auto_deploy = true
-
-#   access_log_settings {
-#     destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-#     format = jsonencode({
-#       requestId               = "$context.requestId"
-#       sourceIp                = "$context.identity.sourceIp"
-#       requestTime             = "$context.requestTime"
-#       protocol                = "$context.protocol"
-#       httpMethod              = "$context.httpMethod"
-#       resourcePath            = "$context.resourcePath"
-#       routeKey                = "$context.routeKey"
-#       status                  = "$context.status"
-#       responseLength          = "$context.responseLength"
-#       integrationErrorMessage = "$context.integrationErrorMessage"
-#     })
-#   }
-# }
-
-# resource "aws_apigatewayv2_integration" "hello_world" {
-#   for_each = toset([for file in fileset("${path.module}/../function/fusionables/**", "handler.js") : basename(dirname(file))])
-#   api_id   = aws_apigatewayv2_api.lambda.id
-
-#   integration_uri    = aws_lambda_function.hello_world[each.value].invoke_arn
-#   integration_type   = "AWS_PROXY"
-#   integration_method = "POST"
-
-#   request_parameters = {
-#     "overwrite:header.X-Amz-Invocation-Type" : "$request.header.InvocationType"
-#   }
-
-#   # request_parameters = {
-#   #   "integration.request.header.X-Amz-Invocation-Type" = "method.request.header.InvocationType"
-#   # }
-# }
-
-# resource "aws_apigatewayv2_route" "hello_world" {
-#   for_each = toset([for file in fileset("${path.module}/../function/fusionables/**", "handler.js") : basename(dirname(file))])
-#   api_id   = aws_apigatewayv2_api.lambda.id
-
-#   route_key = "POST /${each.value}"
-#   target    = "integrations/${aws_apigatewayv2_integration.hello_world[each.value].id}"
-# }
-
-# resource "aws_cloudwatch_log_group" "api_gw" {
-#   name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-
-#   retention_in_days = 1
-# }
-
-# resource "aws_lambda_permission" "api_gw" {
-#   for_each      = toset([for file in fileset("${path.module}/../function/fusionables/**", "handler.js") : basename(dirname(file))])
-#   statement_id  = "AllowExecutionFromAPIGateway"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.hello_world[each.value].function_name
-#   principal     = "apigateway.amazonaws.com"
-
-#   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-# }
-
 // API Gateway v1 for async invocations
 
 // TODO require Author-Header for all Apigateway calls
@@ -256,7 +184,6 @@ module "async_post_endpoint" {
 }
 
 // Add the sync lambda manually since the module apparently only supports async invocation.
-
 resource "aws_api_gateway_method" "sync_proxy" {
   for_each = local.function_names
   rest_api_id = aws_api_gateway_rest_api.api.id
@@ -292,41 +219,3 @@ resource "aws_api_gateway_integration" "sync_lambda_root" {
   type = "AWS_PROXY"
   uri = aws_lambda_function.hello_world[each.value].invoke_arn
 }
-
-# module "sync_post_endpoint" {
-#   source                     = "github.com/vladcar/terraform-aws-serverless-common-api-gateway-method.git"
-#   for_each                   = local.function_names
-#   http_method                = "POST"
-#   integration_type           = "AWS"
-#   integration_http_method    = "POST"
-#   async_response_status_code = "200"
-#   resource_path              = aws_api_gateway_resource.sync_root_resource[each.value].path_part
-#   resource_id                = aws_api_gateway_resource.sync_root_resource[each.value].id
-#   rest_api_id                = aws_api_gateway_rest_api.api.id
-#   validator_id               = aws_api_gateway_request_validator.validator.id
-#   lambda_invoke_arn          = aws_lambda_function.hello_world[each.value].invoke_arn
-#   //enable_async_lambda_integration = false
-# }
-
-// Add the default Response for the sync endpoint since the module does not contain them
-# resource "aws_api_gateway_integration_response" "sync_integration_response" {
-#   for_each = local.function_names
-
-#   depends_on = [
-#     module.sync_post_endpoint
-#   ]
-
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   resource_id = aws_api_gateway_resource.sync_root_resource[each.value].id
-#   http_method = "POST"
-#   status_code = "200"
-
-#   //selection_pattern = "-"
-
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Token'",
-#     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,PATCH,DELETE'",
-#     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-#   }
-# }
-
